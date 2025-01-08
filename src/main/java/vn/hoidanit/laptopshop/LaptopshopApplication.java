f51739bd8c2448dc239357c89046e4663b12edec
mtunsnef <your-email@example.comtuanvip2311>
2025-01-08 14:02:45 +0700

    Add user

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -1,11 +1,15 @@
 package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 
-@SpringBootApplication
-// @SpringBootApplication(exclude =
-// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+//@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class })
+
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -0,0 +1,24 @@
+package vn.hoidanit.laptopshop.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String email;
+    private String password;
+    private String fullName;
+    private String address;
+    private String phone;
+}
